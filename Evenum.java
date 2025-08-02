8a874b3f0ed178a26cce21b5fc7ca15ad4851813
harscini <harscinir@gmail.com>
2025-08-02 19:21:47 +0530

    Evenum.java

---
+++ Evenum.java
@@ -0,0 +1,9 @@
+public class Evennum{
+	public static void main(String[] args){
+		int num=28;
+		if  (num % 2==0){
+		  System.out.Println(number + "is an even number");		
+    }else{
+		  System.out.Println(number + "is an odd number");		  }
+		}
+}
